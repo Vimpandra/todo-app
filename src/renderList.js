8f50ee5135489d311fe40cc1634f5d2ed29a66bd
Vimpandra <victorimperatriz@gmail.com>
2022-12-12 18:07:35 -0300

    Added a delete list button

--- src/renderList.js
+++ src/renderList.js
@@ -121,12 +121,27 @@ function renderList(list) {
         });
     };
 
+    const listControlBtns = document.createElement(`div`);
+    listControlBtns.classList.add(`listControlBtns`);
+    listBody.appendChild(listControlBtns);
+
+    const delListBtn = document.createElement(`button`);
+    delListBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>`;
+    delListBtn.classList.add(`controlBtn`);
+    listControlBtns.appendChild(delListBtn);
+    delListBtn.addEventListener(`click`, () => {
+        if (confirm(`Are you sure you want to delete ${list.title} and all it's content?`)) {
+            listBody.remove();
+        }
+    });
+
     const addItemBtn = document.createElement(`button`);
-    addItemBtn.classList.add(`addItemBtn`);
+    addItemBtn.classList.add(`controlBtn`);
     addItemBtn.textContent = `+`;
-    listBody.appendChild(addItemBtn);
+    listControlBtns.appendChild(addItemBtn);
     addItemBtn.addEventListener(`click`, () => {
         addItemBtn.classList.add(`hidden`);
+        delListBtn.classList.add(`hidden`);
         renderAddListItem(listBody, list);
     });
 };
